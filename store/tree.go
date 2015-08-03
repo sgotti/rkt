094e73d7da1ee8b8a5c3b97e5ce3184639796c3e
Simone Gotti <simone.gotti@gmail.com>
2015-08-03 11:54:23 +0200

    rkt: handle multiple rendered images for the same imageID
    If an image has dependencies, and they have changed, for the same imageID we
    can have multiple different rendered images.
    
    Until now this wasn't handled and rkt and the treestore used the imageID to
    reference the rendered image.
    
    This patch, does multiple things:
    
    * the treestore now returns a treeStoreID on store.RenderTreeStore. The
    treeStoreID is computed as an hash of the flattened dependency tree image keys.
    In this way if some dependencies have changed a new ID will be generated and a
    new image rendering will be done.
    
    * rkt has to use this treeStoreID instead of the imageID.
    
     * for doing this, at prepare phase, the stage1 tree store ID and every app
    treeStoreID is saved in the pod.
    
    Additionally in this patch the prepare phase will create a pod with all the
    needed data for the run phase without the need to keep the needed images in the
    store (excluding the need of the treestore rendered images for overlayfs)
    (previously store.GetImageManifest was called in the run phase).
    
    Now `rkt image rm` just removes the image from the blob store.
    All the rendered images in the tree store aren't touched by `rkt image rm`.
    They will need a future patch for cleaning the unused one (a store gc). This
    will probably add a reference count mechanism to the treestore, since just
    retrieving the treeStoreIDs from the pods is quite racy (due to pods getting
    created between getting the lists and removing the tree store rendered image).
    
    This means that now we can remove any image in the store at any time after the
    prepare phase.

--- store/tree.go
+++ store/tree.go
@@ -40,7 +40,6 @@ const (
 )
 
 // TreeStore represents a store of rendered ACIs
-// The image's key becomes the name of the directory containing the rendered aci.
 type TreeStore struct {
 	path string
 }
@@ -49,8 +48,8 @@ type TreeStore struct {
 // Write, to avoid having a rendered ACI with old stale files, requires that
 // the destination directory doesn't exist (usually Remove should be called
 // before Write)
-func (ts *TreeStore) Write(key string, s *Store) error {
-	treepath := filepath.Join(ts.path, key)
+func (ts *TreeStore) Write(id string, key string, s *Store) error {
+	treepath := filepath.Join(ts.path, id)
 	fi, _ := os.Stat(treepath)
 	if fi != nil {
 		return fmt.Errorf("treestore: path %s already exists", treepath)
@@ -66,7 +65,7 @@ func (ts *TreeStore) Write(key string, s *Store) error {
 	if err != nil {
 		return fmt.Errorf("treestore: cannot render aci: %v", err)
 	}
-	hash, err := ts.Hash(key)
+	hash, err := ts.Hash(id)
 	if err != nil {
 		return fmt.Errorf("treestore: cannot calculate tree hash: %v", err)
 	}
@@ -96,9 +95,9 @@ func (ts *TreeStore) Write(key string, s *Store) error {
 	return nil
 }
 
-// Remove cleans the directory for the specified key
-func (ts *TreeStore) Remove(key string) error {
-	treepath := filepath.Join(ts.path, key)
+// Remove cleans the directory for the specified id
+func (ts *TreeStore) Remove(id string) error {
+	treepath := filepath.Join(ts.path, id)
 	// If tree path doesn't exist we're done
 	_, err := os.Stat(treepath)
 	if err != nil && os.IsNotExist(err) {
@@ -134,10 +133,10 @@ func (ts *TreeStore) Remove(key string) error {
 }
 
 // IsRendered checks if the tree store is fully rendered
-func (ts *TreeStore) IsRendered(key string) (bool, error) {
+func (ts *TreeStore) IsRendered(id string) (bool, error) {
 	// if the "rendered" flag file exists, assume that the store is already
 	// fully rendered.
-	treepath := filepath.Join(ts.path, key)
+	treepath := filepath.Join(ts.path, id)
 	_, err := os.Stat(filepath.Join(treepath, renderedfilename))
 	if os.IsNotExist(err) {
 		return false, nil
@@ -148,25 +147,25 @@ func (ts *TreeStore) IsRendered(key string) (bool, error) {
 	return true, nil
 }
 
-// GetPath returns the absolute path of the treestore for the specified key.
+// GetPath returns the absolute path of the treestore for the specified id.
 // It doesn't ensure that the path exists and is fully rendered. This should
 // be done calling IsRendered()
-func (ts *TreeStore) GetPath(key string) string {
-	return filepath.Join(ts.path, key)
+func (ts *TreeStore) GetPath(id string) string {
+	return filepath.Join(ts.path, id)
 }
 
-// GetRootFS returns the absolute path of the rootfs for the specified key.
+// GetRootFS returns the absolute path of the rootfs for the specified id.
 // It doesn't ensure that the rootfs exists and is fully rendered. This should
 // be done calling IsRendered()
-func (ts *TreeStore) GetRootFS(key string) string {
-	return filepath.Join(ts.GetPath(key), "rootfs")
+func (ts *TreeStore) GetRootFS(id string) string {
+	return filepath.Join(ts.GetPath(id), "rootfs")
 }
 
 // TreeStore calculates an hash of the rendered ACI. It uses the same functions
 // used to create a tar but instead of writing the full archive is just
 // computes the sha512 sum of the file infos and contents.
-func (ts *TreeStore) Hash(key string) (string, error) {
-	treepath := filepath.Join(ts.path, key)
+func (ts *TreeStore) Hash(id string) (string, error) {
+	treepath := filepath.Join(ts.path, id)
 
 	hash := sha512.New()
 	iw := NewHashWriter(hash)
@@ -182,13 +181,13 @@ func (ts *TreeStore) Hash(key string) (string, error) {
 
 // Check calculates the actual rendered ACI's hash and verifies that it matches
 // the saved value.
-func (ts *TreeStore) Check(key string) error {
-	treepath := filepath.Join(ts.path, key)
+func (ts *TreeStore) Check(id string) error {
+	treepath := filepath.Join(ts.path, id)
 	hash, err := ioutil.ReadFile(filepath.Join(treepath, hashfilename))
 	if err != nil {
 		return fmt.Errorf("treestore: cannot read hash file: %v", err)
 	}
-	curhash, err := ts.Hash(key)
+	curhash, err := ts.Hash(id)
 	if err != nil {
 		return fmt.Errorf("treestore: cannot calculate tree hash: %v", err)
 	}
